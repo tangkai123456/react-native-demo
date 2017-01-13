25a457049f31e3d765c721df9fc6e8ea89fd52ff
tangkai123456 <tangkai123456@live.com>
2017-01-13 15:46:46 +0800

    上传的进图条

--- app/account/index.js
+++ app/account/index.js
@@ -23,6 +23,9 @@ import ImagePicker from 'react-native-image-picker'
 import request from "../common/request.js"
 import config from "../common/config.js"
 import sha1 from "sha1"
+import {
+  Circle
+} from "react-native-progress"
 
 var options = {
   title: '选择头像',
@@ -49,6 +52,12 @@ const CLOUDINARY = {
 }
 
 function avatar(id, type) {
+  if (id.indexOf("http") > -1) {
+    return id
+  }
+  if (id.indexOf("data:image") > -1) {
+    return id
+  }
   return CLOUDINARY.base + "/" + type + "/upload/" + id
 }
 
@@ -57,9 +66,13 @@ export default class Account extends Component {
     super(props)
     var user = this.props.user || {}
     this.state = {
-      user: user
+      user: user,
+      avatarProgress: 0,
+      avatarUploading: false
     }
     this._pickPhoto = this._pickPhoto.bind(this)
+    this._upload = this._upload.bind(this)
+    this._asyncUser = this._asyncUser.bind(this)
   }
 
   _pickPhoto() {
@@ -68,6 +81,9 @@ export default class Account extends Component {
       if (res.didCancel) {
         return
       }
+      this.setState({
+        avatarUploading: true
+      })
       var avartarData = "data:image/jpeg;base64," + res.data
       var user = this.state.user
       user.avatar = avartarData
@@ -86,7 +102,6 @@ export default class Account extends Component {
         })
         .then((data) => {
           if (data && data.success) {
-            console.log(data)
             var signature = `folder=${folder}&tags=${tags}&timestamp=${timestamp}${CLOUDINARY.api_secret}`
             signature = sha1(signature)
             var body = new FormData()
@@ -106,32 +121,64 @@ export default class Account extends Component {
     })
   }
   _upload(body) {
+
     var xhr = new XMLHttpRequest()
     var url = CLOUDINARY.image
     xhr.open("post", url)
     xhr.onload = () => {
-      if (xhr.status !== 200) {
-        AlertIOS.alert(xhr.responseText)
-        return
+        if (xhr.status !== 200) {
+          AlertIOS.alert(xhr.responseText)
+          return
+        }
+        if (!xhr.responseText) {
+          AlertIOS.alert("请求失败")
+          return
+        }
+        var res
+        try {
+          res = JSON.parse(xhr.responseText)
+        } catch (e) {
+          AlertIOS.alert(e.message)
+        }
+        if (res && res.public_id) {
+          var user = this.state.user
+          user.avatar = res.public_id
+          this.setState({
+            ...user,
+            avatarUploading: false,
+            avatarProgress: 0
+          })
+          this._asyncUser(true)
+        }
       }
-      if (!xhr.responseText) {
-        AlertIOS.alert("请求失败")
-        return
-      }
-      var res
-      try {
-        res = JSON.parse(xhr.responseText)
-      } catch (e) {
-        AlertIOS.alert(e.message)
-      }
-      if (res && res.public_id) {
-        console.log(res)
-        var user = this.state.user
-        user.avatar = avatar(res.public_id, "image")
-        this.setState(user)
+      // 进度条
+    if (xhr.upload) {
+      xhr.upload.onprogress = (e) => {
+        if (e.lengthComputable) {
+          var per = Number((e.loaded / e.total).toFixed(2))
+          this.setState({
+            avatarProgress: per
+          })
+        }
       }
     }
     xhr.send(body)
+  }
+  _asyncUser(isAvatar) {
+    var url = config.api.base + config.api.update
+    var user = this.state.user
+    request.post(url, user)
+      .then((data) => {
+        if (data && data.success) {
+          var user = data.data
+          if (isAvatar) {
+            AlertIOS.alert("图片更新成功")
+          }
+          this.setState({
+            user: user
+          })
+        }
+      })
   }
   componentDidMount() {
 
@@ -144,14 +191,26 @@ export default class Account extends Component {
           <Text style={styles.toolbarTitle}>我的账户</Text>
         </View>
         {
-          !user.avatar?
+          user.avatar?
             <TouchableOpacity style={styles.avatarContainer} onPress={this._pickPhoto}>
-              <Image source={{uri:user.avatar}} style={styles.avatarContainer}>
+              <Image source={{uri:avatar(user.avatar,"image")}} style={styles.avatarContainer}>
                 <View style={styles.avatarBox}>
-                  <Image
-                   source={{uri:user.avatar}}
-                   style={styles.avatar}/>
-                  <Text style={styles.avatarTip}>戳这里换头像</Text>
+                {this.state.avatarUploading?
+                  <Circle
+                    showsText={true}
+                    size={75}
+                    color="#ee735c"
+                    progress={this.state.avatarProgress}
+                  />
+                  :
+                  <View>
+                    <Image
+                     source={{uri:avatar(user.avatar,"image")}}
+                     style={styles.avatar}/>
+                    <Text style={styles.avatarTip}>戳这里换头像</Text>
+                  </View>
+                }
+                  
                 </View>
               </Image>
             </TouchableOpacity>
@@ -159,9 +218,18 @@ export default class Account extends Component {
             <TouchableOpacity style={styles.avatarContainer} onPress={this._pickPhoto}>
               <Text style={styles.avatarTip}>添加狗狗头像</Text>
               <View style={styles.avatarBox}>
+              {this.state.avatarUploading?
+                <Circle
+                  showsText={true}
+                  size={75}
+                  color="#ee735c"
+                  progress={this.state.avatarProgress}
+                />
+                :
                 <Icon
                   name="ios-cloud-upload-outline"
                   style={styles.plusIcon}/>
+              }
               </View>
             </TouchableOpacity>
         }
